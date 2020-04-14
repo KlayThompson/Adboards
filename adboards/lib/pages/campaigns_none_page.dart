82f4e0b0b5213128a57e04762e8b2b498f28b4bf
刘峰 <913661647@qq.com>
2020-04-14 21:09:46 +0800

    campaigns none page finish

--- adboards/lib/pages/campaigns_none_page.dart
+++ adboards/lib/pages/campaigns_none_page.dart
@@ -1,12 +1,16 @@
+import 'package:adboards/widget/main_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CampaignsNonePage extends StatelessWidget {
 
+  final String avatarAsset = 'resources/img/avatar.png';
+  final String bgImgAsset = 'resources/img/undraw_winter_magic_5xu2.png';
+  final String addAsset = 'resources/img/add.png';
+
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
     return Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: false,
@@ -15,8 +19,17 @@ class CampaignsNonePage extends StatelessWidget {
           elevation: 0,
           backgroundColor: Colors.white,
           brightness: Brightness.light,
+          actions: <Widget>[
+            _avatarButtonWidget()
+          ],
         ),
-        body: Center(child: Text('text'),)
+        body: ListView(
+          children: <Widget>[
+            _bodyImgWidget(),
+            _centerTextWidget(),
+            _createCampaignsButtonWidget(context)
+          ],
+        )
     );
   }
   
@@ -39,6 +52,66 @@ class CampaignsNonePage extends StatelessWidget {
           ],
         ),
       ],
+    );
+  }
+
+  Widget _avatarButtonWidget() {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.only(right: 20),
+        child: CircleAvatar(
+          child: Image.asset(avatarAsset,height: 45,),
+        ),
+      ),
+    );
+  }
+  
+  Widget _bodyImgWidget() {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.white, Color.fromARGB(1, 250, 250, 250)],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter
+        )
+      ),
+      padding: EdgeInsets.fromLTRB(38, 90, 38, 60),
+      child: Image.asset(bgImgAsset),
+    );
+  }
+
+  Widget _centerTextWidget() {
+    return Container(
+      padding: EdgeInsets.only(left: 35,right: 35,bottom: 90),
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Start by Creating a Campaign',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: ScreenUtil().setSp(28),
+            ),
+          ),
+          SizedBox(height: 12,),
+          Text(
+            'Don’t worry, It’s easy & takes only a few minutes',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color.fromRGBO(68, 68, 68, 0.6),
+              fontSize: ScreenUtil().setSp(15)
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _createCampaignsButtonWidget(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: MainButtonWidget(icon: addAsset,title: 'Create a Campaign'),
     );
   }
 }
